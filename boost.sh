9e204c3e4a822246fb80ba19ac2ad3f4fff0cdae
genx7up <genx7up@users.noreply.github.com>
2014-09-17 21:27:35 -0700

    Update boost.sh

--- boost.sh
+++ boost.sh
@@ -1,38 +1,60 @@
-yum -y install bzip2-devel libtool
+yum -y install bzip2-devel libtool gflags-devel libevent-devel libcap-devel openssl-devel
 
+#CMAKE
 cd /opt && wget http://www.cmake.org/files/v2.8/cmake-2.8.12.2.tar.gz
 tar xvf cmake-2.8.12.2.tar.gz && cd cmake-2.8.12.2
 ./configure && make && make install
 
+#GLOG
+cd /opt && wget https://google-glog.googlecode.com/files/glog-0.3.3.tar.gz
+tar xvf glog-0.3.3.tar.gz && cd glog-0.3.3
+./configure && make && make install
+
+#COLM for RAGEL
 cd /opt && git clone https://github.com/ehdtee/colm.git
 cd colm && ./autogen.sh
 ./configure
 make && make install
 
+#RAGEL
 cd /opt && git clone https://github.com/ehdtee/ragel.git
 cd ragel && ./autogen.sh
 ./configure --prefix=/usr --disable-manual
 make && make install
 
-cd /opt && wget https://google-glog.googlecode.com/files/glog-0.3.3.tar.gz
-tar xvf glog-0.3.3.tar.gz && cd glog-0.3.3
-./configure && make && make install
+#SCONS & double-conversion for Folly
+rpm -Uvh http://sourceforge.net/projects/scons/files/scons/2.3.3/scons-2.3.3-1.noarch.rpm
+cd /opt && git clone https://code.google.com/p/double-conversion/
+cd double-conversion && scons install
 
-cd /opt && git clone https://code.google.com/p/gflags/
-cd gflags && mkdir build && cd build
-ccmake ..
+#Folly
+cd /opt && git clone https://github.com/genx7up/folly.git
+cp folly/folly/SConstruct.double-conversion /opt/double-conversion/
+cd double-conversion && scons -f SConstruct.double-conversion
+mkdir /opt/double-conversion/double-conversion/
+cp -r /opt/double-conversion/src/* /opt/double-conversion/double-conversion/
+
+#Folly
+cd /opt/folly/folly/
+export LDFLAGS=-L/opt/double-conversion/
+export CPPFLAGS=-I/opt/double-conversion/
+autoreconf -ivf
+./configure
 make && make install
 
+#Python27 for Boost
 yum -y install centos-release-SCL
 yum -y install python27
 scl enable python27 "easy_install pip"
 
+#Boost
 scl enable python27 bash
 cd /opt && wget http://downloads.sourceforge.net/boost/boost_1_56_0.tar.bz2
 tar jxf boost_1_56_0.tar.bz2 && cd boost_1_56_0
 ./bootstrap.sh --prefix=/usr && ./b2 stage threading=multi link=shared
 ./b2 install threading=multi link=shared
 
+#McRouter
 cd /opt && git clone https://github.com/facebook/mcrouter.git
 cd mcrouter/mcrouter && autoreconf --install
 ./configure
