9e623835bb76308aa22dd74db40e684c4e1834f0
genx7up <genx7up@users.noreply.github.com>
2014-09-17 18:58:14 -0700

    Update boost.sh

--- boost.sh
+++ boost.sh
@@ -1,13 +1,8 @@
 yum -y install bzip2-devel libtool
 
-yum -y install centos-release-SCL
-yum -y install python27
-scl enable python27 "easy_install pip"
-
-cd /opt && wget http://downloads.sourceforge.net/boost/boost_1_56_0.tar.bz2
-tar jxf boost_1_56_0.tar.bz2 && cd boost_1_56_0
-scl enable python27 "./bootstrap.sh --prefix=/usr && ./b2 stage threading=multi link=shared"
-scl enable python27 "./b2 install threading=multi link=shared"
+cd /opt && wget http://www.cmake.org/files/v2.8/cmake-2.8.12.2.tar.gz
+tar xvf cmake-2.8.12.2.tar.gz && cd cmake-2.8.12.2
+./configure && make && make install
 
 cd /opt && git clone https://github.com/ehdtee/colm.git
 cd colm && ./autogen.sh
@@ -23,12 +18,20 @@ cd /opt && wget https://google-glog.googlecode.com/files/glog-0.3.3.tar.gz
 tar xvf glog-0.3.3.tar.gz && cd glog-0.3.3
 ./configure && make && make install
 
-cd /opt && wget http://www.cmake.org/files/v2.8/cmake-2.8.12.2.tar.gz
-tar xvf glog-0.3.3.tar.gz && cd glog-0.3.3
-
-
 cd /opt && git clone https://code.google.com/p/gflags/
-cd gflags && 
+cd gflags && mkdir build && cd build
+ccmake ..
+make && make install
+
+yum -y install centos-release-SCL
+yum -y install python27
+scl enable python27 "easy_install pip"
+
+scl enable python27 bash
+cd /opt && wget http://downloads.sourceforge.net/boost/boost_1_56_0.tar.bz2
+tar jxf boost_1_56_0.tar.bz2 && cd boost_1_56_0
+./bootstrap.sh --prefix=/usr && ./b2 stage threading=multi link=shared
+./b2 install threading=multi link=shared
 
 cd /opt && git clone https://github.com/facebook/mcrouter.git
 cd mcrouter/mcrouter && autoreconf --install
