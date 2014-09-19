5efecb047e2b537541413873d0c3ebae7c9c3e38
genx7up <genx7up@users.noreply.github.com>
2014-09-18 23:09:29 -0700

    Update boost.sh

--- boost.sh
+++ boost.sh
@@ -1,4 +1,4 @@
-yum -y install bzip2-devel libtool gflags-devel libevent-devel libcap-devel openssl-devel 
+yum -y install unzip bzip2-devel libtool gflags-devel libevent-devel libcap-devel openssl-devel 
 yum -y bison flex snappy-devel numactl-devel cyrus-sasl-devel
 
 #CMAKE
@@ -35,6 +35,7 @@ scl enable python27 "easy_install pip"
 
 #Boost
 scl enable python27 bash
+python --version
 cd /opt && wget http://downloads.sourceforge.net/boost/boost_1_56_0.tar.bz2
 tar jxf boost_1_56_0.tar.bz2 && cd boost_1_56_0
 ./bootstrap.sh --prefix=/usr && ./b2 stage threading=multi link=shared
@@ -61,6 +62,11 @@ autoreconf -ivf
 ./configure
 make && make install
 
+#Folly Test
+cd /opt/folly/folly/test
+wget https://googletest.googlecode.com/files/gtest-1.6.0.zip
+unzip gtest-1.6.0.zip
+
 #Thrift
 cd /opt && git clone https://github.com/facebook/fbthrift.git
 cd fbthrift/thrift
@@ -69,6 +75,7 @@ export LD_LIBRARY_PATH="/opt/fbthrift/thrift/lib:$LD_LIBRARY_PATH"
 export LD_RUN_PATH="/opt/fbthrift/thrift/lib"
 export LDFLAGS="-L/opt/fbthrift/thrift/lib -L/usr/local/lib"
 export CPPFLAGS="-I/opt/fbthrift/thrift/include -I/opt/fbthrift/thrift/include/python2.7 -I/opt/folly -I/opt/double-conversion"
+echo "/usr/local/lib/" >> /etc/ld.so.conf.d/gcc-4.9.1.conf && ldconfig
 /usr/local/bin/autoreconf -ivf
 ./configure --enable-boostthreads
 cd /opt/fbthrift/thrift/compiler && make
@@ -79,7 +86,11 @@ cd /opt/fbthrift/thrift && make && make install
 
 #McRouter
 cd /opt && git clone https://github.com/facebook/mcrouter.git
-cd mcrouter/mcrouter && autoreconf --install
-./configure
+cd mcrouter/mcrouter
+export LD_LIBRARY_PATH="/opt/mcrouter/mcrouter/lib:$LD_LIBRARY_PATH"
+export LD_RUN_PATH="/opt/folly/folly/test/.libs:/opt/mcrouter/mcrouter/lib"
+export LDFLAGS="-L/opt/mcrouter/mcrouter/lib -L/usr/local/lib -L/opt/folly/folly/test/.libs"
+export CPPFLAGS="-I/opt/folly/folly/test/gtest-1.6.0/include -I/opt/mcrouter/mcrouter/include -I/opt/folly -I/opt/double-conversion -I/opt/fbthrift -fpermissive"
+autoreconf --install && ./configure
 make && make install
 mcrouter --help
